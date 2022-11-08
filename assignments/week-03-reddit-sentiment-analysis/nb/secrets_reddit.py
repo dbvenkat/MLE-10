e06de61769cbf6df9958b35e25a6b72bd70c8a61
Swaminathan Venkataraman <venkat.db@gmail.com>
2022-11-07 20:40:10 -0600

    Worked on the assignment for week 3

---
+++ assignments/week-03-reddit-sentiment-analysis/nb/secrets_reddit.py
@@ -0,0 +1,5 @@
+reddit_secrets = {
+    "REDDIT_API_CLIENT_ID": "aXC6KvUIsJXQ8Y4sOER4mA",
+    "REDDIT_API_CLIENT_SECRET": "v_VIwjI9Ns8vzmQ9GvneWAxD5J8KZg",
+    "REDDIT_API_USER_AGENT": "python 3.8: db_fb_mle_script"
+}
